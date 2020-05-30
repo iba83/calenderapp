76c099e20167b789470c319f1aab2fb675d2a39d
iba83 <sson.joer@icloud.com>
2020-05-30 19:53:28 +0900

    Initial Commit

---
+++ calenderapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calenderapp
+//
+//  Created by 射場智子 on 2020/05/30.
+//  Copyright © 2020 射場智子. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
